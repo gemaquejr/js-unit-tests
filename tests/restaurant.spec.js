2190ee0dfc22da3052c65f0bd769a79a10af4108
gemaquejr <gemaquejr@hotmail.com>
2023-03-07 16:48:29 -0300

    Adiciona teste5 do objetoRetornado

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -89,6 +89,9 @@ describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
     // objetoRetornado.order("coxinha");
     // objetoRetornado.consumption // Retorno: ["coxinha"]
     // ```
+    const objetoRetornadoTeste5 = createMenu(objetoQualquer);
+    objetoRetornadoTeste5.order('coxinha');
+    expect(objetoRetornadoTeste5.consumption).toEqual(['coxinha']);
     // Agora faça o PASSO 3 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 6: Verifique se, ao adicionar três pedidos, dentre bebidas e comidas, o array `objetoRetornado.consumption` contém os itens pedidos.
