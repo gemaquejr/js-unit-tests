8a8dba74538aa57844fb1b5b58f3265511076a05
gemaquejr <gemaquejr@hotmail.com>
2023-03-07 16:42:22 -0300

    Adiciona teste1 objetoRetornado

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -46,12 +46,15 @@ const createMenu = require('../src/restaurant');
 
 describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
+    const objetoQualquer = { food: { 'coxinha': 3.9, 'sopa': 9.9 }, drink: { 'agua': 3.9, 'cerveja': 6.9 } };
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui a
     // chave fetchMenu, a qual tem como valor uma função.
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
+    const objetoRetornadoTeste1 = createMenu();
+    expect(typeof objetoRetornadoTeste1.fetchMenu).toEqual('function');
     // TESTE 2: Verifique se 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`, 
     // considerando que a função createMenu() foi chamada com o objeto: `{ food: {}, drink: {} }`.
     // ```
